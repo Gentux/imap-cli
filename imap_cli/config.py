60444ca815d0ae58babc1238631d587ac0709787
Romain Soufflet <romain@soufflet.io>
2014-07-24 11:59:51 +0200

    Load context by python dict in addition to file loading

--- imap_cli/config.py
+++ imap_cli/config.py
@@ -13,6 +13,24 @@ from imap_cli import const
 
 
 app_name = os.path.splitext(os.path.basename(__file__))[0]
+DEFAULT_CONFIG = {
+    'hostname': 'imap.example.org',
+    'username': 'username',
+    'password': 'secret',
+    'ssl': True,
+
+    'limit': 10,
+    'format_list': "".join([
+        "\n",
+        "ID:         {mail_id}\n",
+        "Flags:      {flags}\n",
+        "From:       {mail_from}\n",
+        "To:         {to}\n",
+        "Date:       {date}\n",
+        "Subject:    {subject}",
+    ]),
+    'format_status': "{directory:>20} : {count:>5} Mails - {unseen:>5} Unseen - {recent:>5} Recent"
+}
 log = logging.getLogger(app_name)
 
 
@@ -26,6 +44,18 @@ class Ctx(object):
     limit = None
     format_status = None
     format_list = None
+
+
+def new_context(config=None):
+    ctx = Ctx()
+    if config is None:
+        log.debug("Loading default configuration")
+        config = DEFAULT_CONFIG
+    else:
+        log.debug("Loading custom configuration")
+    for key, value in config.items():
+        setattr(ctx, key, value)
+    return ctx
 
 
 def new_context_from_file(config_filename=None):
