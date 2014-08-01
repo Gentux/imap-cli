79f1134d9d951f0726f756487c30b4a0626b7e82
Romain Soufflet <romain@soufflet.io>
2014-08-01 11:44:45 +0200

    Change "read" command to display human readable mail

--- examples/summary.py
+++ examples/summary.py
@@ -57,7 +57,8 @@ def main():
             mail_set = search.search(ctx, search_criterion=[search.create_search_criteria_by_tag(['unseen'])])
 
             for mail_info in list_mail.list_mail(ctx, directory=directory_status['directory'], mail_set=mail_set):
-                sys.stdout.write(u'    From : {:<30} \tSubject : {}\n'.format(
+                sys.stdout.write(u'    {:<10} From : {:<30} \tSubject : {}\n'.format(
+                    mail_info['uid'],
                     truncate_string(mail_info['mail_from'], 30),
                     truncate_string(mail_info['subject'], 50),
                 ))
--- imap_cli/read.py
+++ imap_cli/read.py
@@ -28,7 +28,9 @@ import sys
 import docopt
 
 from imap_cli import config
+from imap_cli import const
 from imap_cli.imap import connection
+from imap_cli.imap import directories
 from imap_cli.imap import fetch
 
 
@@ -36,9 +38,43 @@ log = logging.getLogger('imap-cli-read')
 
 
 def read(ctx, mail_uid, directory=None):
-    # TODO(rsoufflet)  this is realy ugly, thinking of an elegant solution
-    status, mail_count = ctx.mail_account.select(directory, True)
-    return fetch.fetch(ctx, [mail_uid])[0][1]
+    """Return mail information within a dict"""
+    assert directories.change_dir(ctx, directory or const.DEFAULT_DIRECTORY) >= 0
+
+    raw_mail = fetch.fetch(ctx, [mail_uid])[0][1]
+    mail = email.message_from_string(raw_mail)
+
+    mail_headers = {}
+    for header_name, header_value in mail.items():
+        value, encoding = header.decode_header(header_value)[0]
+        if encoding is not None:
+            value = value.decode(encoding)
+        elif not isinstance(value, unicode):
+            value = unicode(value)
+        mail_headers[header_name] = value
+
+    message_parts = []
+    for part in mail.walk():
+        # multipart/* are just containers
+        if part.get_content_maintype() == 'multipart':
+            continue
+
+        if part.get_content_type().startswith('text'):
+            message_parts.append({
+                'content_type': part.get_content_type(),
+                'data': part.as_string(),
+            })
+        elif part.get_filename():
+            message_parts.append({
+                'content_type': part.get_content_type(),
+                'filename': part.get_filename(),
+                'data': part.get_payload(decode=True),
+            })
+
+    return {
+        'headers': mail_headers,
+        'parts': message_parts,
+    }
 
 
 def main():
@@ -51,10 +87,23 @@ def main():
     ctx = config.new_context_from_file(args['--config-file'])
 
     connection.connect(ctx)
-    mail = email.message_from_string(read(ctx, args['<mail_uid>']))
-    for header_name, header_value in mail.items():
-        mail.replace_header(header_name, header.decode_header(header_value)[0][0])
-    sys.stdout.write(mail.as_string())
+    mail_data = read(ctx, args['<mail_uid>'])
+
+    sys.stdout.write('\n'.join([
+        u'From       : {}'.format(mail_data['headers']['From']),
+        u'Subject    : {}'.format(mail_data['headers']['Subject']),
+        u'Date       : {}'.format(mail_data['headers']['Date']),
+        u'',
+        u'\n\n'.join([part['data'] for part in mail_data['parts'] if part['content_type'] == 'text/plain']),
+    ]))
+    other_parts = [part for part in mail_data['parts'] if not part['content_type'].startswith('text')]
+    if len(other_parts) > 0:
+        sys.stdout.write('\nAttachement :\n')
+        for part in other_parts:
+            sys.stdout.write('    ')
+            sys.stdout.write(part.get('filename', part.get('content_type', '')))
+            sys.stdout.write('\n')
+
     return 0
 
 if __name__ == '__main__':
