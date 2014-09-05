adbd73414a1ca3b7bbc0ddf0bff25eb44589fa13
Romain Soufflet <romain@soufflet.io>
2014-09-05 14:01:31 +0200

    Fix config tests

--- imap_cli/tests/test_config.py
+++ imap_cli/tests/test_config.py
@@ -8,6 +8,7 @@ import json
 import unittest
 
 from imap_cli import config
+from imap_cli import const
 
 
 class ConfigTest(unittest.TestCase):
@@ -28,7 +29,19 @@ class ConfigTest(unittest.TestCase):
             assert self.conf[key] == value
 
     def test_config_file_from_default_config_file(self):
+        const.DEFAULT_CONFIG_FILE = 'config-example.ini'
+
         self.conf = config.new_context_from_file()
+
+        if self.conf is not None:
+            for key, value in config.DEFAULT_CONFIG.items():
+                assert self.conf[key] == value
+
+    def test_config_file_from_non_existing_file(self):
+        const.DEFAULT_CONFIG_FILE = 'config-imaginary-file.ini'
+
+        self.conf = config.new_context_from_file()
+
         if self.conf is not None:
             for key, value in config.DEFAULT_CONFIG.items():
                 assert self.conf[key] == value
